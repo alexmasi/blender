f4ef5ec4c33ddb6aa9eab4673a8debb4152e9a36
Alex Masi <a3masi@gmail.com>
2018-09-24 22:43:54 -0400

    Try alternate clustering method

--- backend/blend.py
+++ backend/blend.py
@@ -8,7 +8,7 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 
 
 json_string = sys.argv[1]
-# with open('./three_users.txt') as infile:
+# with open('./two_users.txt') as infile:
 #     json_string = infile.read().strip("'\n")
 
 song_list = json.loads(json_string)
@@ -20,20 +20,21 @@ for user in song_list:
     for song in user['songs']:
         tracks.append([
             song['danceability'],
-            song['energy'],
+            song['energy']])
             # song['key'],
-            song['loudness'],
-            song['mode'],
-            song['speechiness'],
-            song['acousticness'],
-            song['instrumentalness'],
-            song['liveness'],
-            song['valence'],  # how positive the mood is
-            song['tempo']])
+            # song['loudness'],
+            # song['mode']])
+            # song['speechiness'],
+            # song['acousticness'],
+            # song['instrumentalness'],
+            # song['liveness'],
+            # song['valence']])  # how positive the mood is
+            # song['tempo']])
         tracks_uri.append(song['uri'])
 
 
-Z = linkage(tracks, 'single')
+
+Z = linkage(tracks, 'average')
 cluster_list = []
 for i in range(len(tracks)-1):
     if Z[i, 3] >= (len(song_list)*10):
@@ -42,9 +43,29 @@ for i in range(len(tracks)-1):
         break
 
 
-# fig = plt.figure(figsize=(15, 5))
-# dn = dendrogram(Z, labels=tracks_uri)
-# plt.show()
+# closest_clusters = []
+# midpoint = len(tracks) // len(song_list)
+# for i in range(len(tracks)-1):
+#     if Z[i, 0] < len(tracks) and Z[i, 1] < len(tracks):
+#         if Z[i, 0] < midpoint:
+#             if Z[i, 1] >= midpoint:
+#                 closest_clusters.append(Z[i])
+#         else:
+#             if Z[i, 1] < midpoint:
+#                 closest_clusters.append(Z[i])
+
+# total_size = 0
+# for c in closest_clusters:
+#     print(c)
+#     total_size += c[3]
+#     print(total_size)
+
+# final_uris = []
+# for c in closest_clusters:
+#     final_uris.append(tracks_uri[int(c[0])])
+#     final_uris.append(tracks_uri[int(c[1])])
+#
+# print(final_uris)
 
 for cluster in cluster_list:
     if cluster >= len(tracks):
@@ -56,3 +77,7 @@ final_tracks = [tracks_uri[c] for c in cluster_list if c < len(tracks)]
 # remove dups
 final_tracks_no_dups = list(dict.fromkeys(final_tracks))
 print(json.dumps(final_tracks_no_dups))
+
+# fig = plt.figure(figsize=(15, 5))
+# dn = dendrogram(Z)#, labels=tracks_uri)
+# plt.show()
